b201e171be874f60a0388725c39c295073af086f
brian cajes <brian@empiricotx.com>
2021-08-01 11:56:57 -0700

    increasing max iteration and decreasing step size for null fit
    Signed-off-by: brian cajes <brian@empiricotx.com>

--- python/glow/gwas/approx_firth.py
+++ python/glow/gwas/approx_firth.py
@@ -162,7 +162,7 @@ def perform_null_firth_fit(
         b0_null_fit[0] = (0.5 + masked_y.sum()) / (mask.sum() + 1)
         b0_null_fit[0] = np.log(b0_null_fit[0] / (1 - b0_null_fit[0])) - masked_offset.mean()
     #TODO review increasing max iterations for null fit, due to encountering failed null fits in prod, originally 250
-    firth_fit_result = _fit_firth(b0_null_fit, masked_C, masked_y, masked_offset, max_iter=2000)
+    firth_fit_result = _fit_firth(b0_null_fit, masked_C, masked_y, masked_offset, max_step_size=1, max_iter=10000)
     if firth_fit_result is None:
         raise ValueError("Null fit failed!")
     firth_offset[mask] = masked_offset + masked_C @ firth_fit_result.beta
